fa96ae1fbdf7d8ee732276c246da09281b6638de
alberto romano <aromanog1@gmail.com>
2023-03-10 10:23:31 +0100

    home page hero section almost done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { PredictPageComponent } from './pages/predict-page/predict-page.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent, PredictPageComponent],
@@ -15,6 +16,7 @@ import { PredictPageComponent } from './pages/predict-page/predict-page.componen
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,100 +1,11 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent implements AfterViewInit {
-  @ViewChild('firstCard') first_card: any;
-  @ViewChild('secondCard') second_card: any;
-  @ViewChild('topCard') top_card: any;
-  @ViewChild('thirdCard') third_card: any;
-  @ViewChild('fourthCard') fourth_card: any;
-
-  @ViewChild('imageInput') imageInput: any;
-
-  imageUploaded: File | null = null;
-
-  animationFinished: boolean = false;
-
-  async ngAfterViewInit(): Promise<void> {
-    const cards: any[] = [
-      this.first_card,
-      this.second_card,
-      this.top_card,
-      this.third_card,
-      this.fourth_card,
-    ];
-
-    this.imageInput.nativeElement.addEventListener('change', () => {
-      this.imageUploaded = this.imageInput.nativeElement.files[0];
-    });
-
-    while (true) {
-      if (!this.imageUploaded) {
-        await new Promise((r) => setTimeout(r, 1000));
-        rotateCards(cards);
-      } else {
-        // TODO: This should be changed when the API returns the prediction
-        for (let i = 1000; i > 200; i -= 100) {
-          await new Promise((r) => setTimeout(r, i));
-          rotateCards(cards);
-        }
-
-        for (let i = 0; i < 25; i++) {
-          await new Promise((r) => setTimeout(r, 150));
-          rotateCards(cards);
-        }
-        shrinkCards(cards);
-        for (let i = 0; i < 25; i++) {
-          await new Promise((r) => setTimeout(r, 150));
-          if (i == 3) {
-            this.animationFinished = true;
-          }
-        }
-
-        break;
-      }
-    }
-  }
-}
-
-function shrinkCards(cards: any[]) {
-  cards.forEach((card: any) => {
-    card.nativeElement.classList.remove('big-card');
-    card.nativeElement.classList.add('small-card');
-  });
-}
-
-function rotateCards(cards: any[]) {
-  const classes: any = {
-    0: 'first-card',
-    1: 'second-card',
-    2: 'top-card',
-    3: 'third-card',
-    4: 'fourth-card',
-  };
-
-  cards.forEach((card: any) => {
-    card = card.nativeElement;
-    let i = Number.parseInt(card.dataset.position);
-
-    if (i >= 4) {
-      i = 0;
-    } else {
-      i++;
-    }
-    if (card.classList.contains('small-card')) {
-      card.classList = ['card-image'];
-      card.classList.add('small-card');
-    } else {
-      console.log('Big card added somehow');
-      card.classList = ['card-image'];
-      card.classList.add('big-card');
-    }
-    card.classList.add(classes[i]);
-
-    card.dataset.position = i;
-  });
+export class HomePageComponent {
+  faGithub = faGithub;
 }
