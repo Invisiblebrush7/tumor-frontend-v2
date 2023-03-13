7c309871f5ac323563809137111f929ad716bff1
alberto romano <aromanog1@gmail.com>
2023-03-13 16:45:40 +0100

    Comitting before this thing breaks something just by being that old

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { PredictPageComponent } from './pages/predict-page/predict-page.component';
+import { TensorSpacePageComponent } from './pages/tensor-space-page/tensor-space-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   {
     path: 'predict',
     component: PredictPageComponent,
+  },
+  {
+    path: 'tensorspace',
+    component: TensorSpacePageComponent,
   },
 ];
 
--- src/app/pages/tensor-space-page/tensor-space-page.component.ts
+++ src/app/pages/tensor-space-page/tensor-space-page.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ApiServiceService } from 'src/app/services/api-service.service';
 
 @Component({
   selector: 'app-tensor-space-page',
   templateUrl: './tensor-space-page.component.html',
-  styleUrls: ['./tensor-space-page.component.scss']
+  styleUrls: ['./tensor-space-page.component.scss'],
 })
 export class TensorSpacePageComponent {
-
+  // ngAfterViewInit() {
+  //   const container = document.getElementById('container');
+  //   const model = new TSP.models.Sequential(container);
+  //   model.add(new TSP.layers.GreyscaleInput());
+  //   model.add(new TSP.layers.Padding2d());
+  //   model.add(new TSP.layers.Conv2d());
+  //   model.add(new TSP.layers.Pooling2d());
+  //   model.add(new TSP.layers.Conv2d());
+  //   model.add(new TSP.layers.Pooling2d());
+  //   model.add(new TSP.layers.Dense());
+  //   model.add(new TSP.layers.Dense());
+  //   model.add(
+  //     new TSP.layers.Output1d({
+  //       outputs: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  //     })
+  //   );
+  //   // Load model
+  //   model.load({
+  //     type: 'tfjs',
+  //     url: './assets/model/mnist.json',
+  //     onComplete: function () {
+  //       console.log('"Hello Angular!" from TensorSpace Loader.');
+  //     },
+  //   });
+  //   // Load data
+  //   model.init(function () {
+  //     fetch('./assets/data/5.json')
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         model.predict(data);
+  //       });
+  //   });
+  // }
 }
